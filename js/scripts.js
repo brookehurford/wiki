98398b64a36aaa2240db235e25f11f400347d5e6
Anna Becker and Brooke Hurford <brookebhurford@gmail.com>
2016-01-14 15:29:07 -0800

    Add brancing, array, and looping pages.

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,48 @@ $(document).ready(function(){
     $("ul.unstyled").append('<img src="img/flower.jpg">');
   });
 
-  $("form#some-form").submit(function(event) {
-    var yourName = $("input#yourName").val()
-    var yourNumber = $("input#yourNumber").val()
-    var yourAge = $("select#yourAge").val()
+  $("#blanks form").submit(function(event) {
+    var blanks = ["yourName", "yourNumber", "yourAge"];
 
-    $(".yourName").append(yourName);
-    $(".yourNumber").append(yourNumber);
-    $(".yourAge").append(yourAge);
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      var userSelect = $("select#" + blank).val();
+      $("." + blank).append(userInput);
+      $("." + blank).append(userSelect);
+    })
 
     $("#yourInfo").show();
+    event.preventDefault();
+  });
 
+  $("button#green").click(function() {
+    $("p").removeClass();
+    $("p").addClass("green-text");
+  });
+
+  $("button#yellow").click(function() {
+    $("p").removeClass();
+    $("p").addClass("yellow-text");
+  });
+
+  $("button#red").click(function() {
+    $("p").removeClass();
+    $("p").addClass("red-text");
+  });
+
+  $("form#activities").submit(function(event) {
+    debugger;
+    var userAge = $("input#userAge").val()
+
+    if (userAge <= 20){
+     $(".playground").show();
+     }
+     else if (userAge >= 21 && userAge <= 35) {
+      $(".bar").show();
+     }
+     else {
+     $(".museum").show();
+     }
   event.preventDefault();
   });
 });
